0ce08e919701b85562a482ea06b5a0e1c584c511
Shivam Paul <shivampaul2319@gmail.com>
2025-10-24 19:45:10 +0530

    basic node app

---
+++ calc.js
@@ -0,0 +1,26 @@
+const changeToNum = (a) => Number(a);
+
+export const sum = (a, b) => {
+    a = changeToNum(a);
+    b = changeToNum(b);
+    return a + b;
+};
+
+export const subtract = (a, b) => {
+    a = changeToNum(a);
+    b = changeToNum(b);
+    return a - b;
+};
+
+export const multiply = (a, b) => {
+    a = changeToNum(a);
+    b = changeToNum(b);
+    return a * b;
+};
+
+export const divide = (a, b) => {
+    if(b == 0) return "not defined";
+    a = changeToNum(a);
+    b = changeToNum(b);
+    return a - b;
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { sum, subtract, multiply, divide } from "./calc.js";
+
+const app = express();
+
+app.listen("8080", (req, res) => {
+    console.log("listening");
+});
+
+app.get("/sum/:a/:b", (req, res) => {
+    let {a, b} = req.params;
+    res.send(`Here it is: ${sum(a, b)}`);
+});
+
+app.get("/subt/:a/:b", (req, res) => {
+    let {a, b} = req.params;
+    res.send(`Here it is: ${subtract(a, b)}`);
+});
+
+app.get("/mult/:a/:b", (req, res) => {
+    let {a, b} = req.params;
+    res.send(`Here it is: ${multiply(a, b)}`);
+});
+
+app.get("/div/:a/:b", (req, res) => {
+    let {a, b} = req.params;
+    res.send(`Here it is: ${divide(a, b)}`);
+});
